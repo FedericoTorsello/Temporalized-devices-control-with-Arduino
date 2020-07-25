22cd9cec825ac96450bb866cc999bab03ebb8f31
federico <applegino9>
2020-07-26 01:21:25 +0200

    removed comments

--- src/main.cpp
+++ src/main.cpp
@@ -6,28 +6,8 @@ using namespace Reactive;
 
 //WaterPump waterPump;
 
-int incomingByte = 0; // for incoming serial data
-
-const int pin13 = LED_BUILTIN;
 const int pin12 = 12;
 
-/*
-void setup()
-{
-  pinMode(pin13, OUTPUT);
-  digitalWrite(pin13, LOW);
-
-  pinMode(pin12, INPUT);
-  digitalWrite(pin12, LOW);
-
-  Serial.begin(9600);
-
-  while (!Serial) delay(1); // wait for serial port to connect. Needed for native USB
-
-  Serial.print("Start");
-  
-}
-*/
 auto reactiveInput = Reactive::FromDigitalInput(pin12, INPUT_PULLUP);
 
 auto observableInt = Reactive::Property<int>();
