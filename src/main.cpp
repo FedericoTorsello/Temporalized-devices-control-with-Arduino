d49bd8c63d438c72160068fd724e1f1dfdab52d8
federico <applegino9>
2020-08-01 19:21:42 +0200

    generic task

--- src/main.cpp
+++ src/main.cpp
@@ -1,82 +1,49 @@
 #include <Arduino.h>
-//#include "task/WaterPump.h"
-#include "ReactiveArduinoLib.h"
 
-using namespace Reactive;
+const uint8_t pin2 = 2;
+const uint8_t pin4 = 4;
+const uint8_t pin7 = 7;
+const uint8_t pin8 = 8;
+const uint8_t pin12 = 12;
 
-//WaterPump waterPump;
+const long period = 1000; // period at which to blink in ms
 
-/*const int pin12 = 11;
+unsigned long previousMillis; //will store last time LED was blinked
+unsigned long currentMillis;
 
-auto reactiveInput = Reactive::FromDigitalInput(pin12, INPUT_PULLUP);
-
-auto observableIntSerialOutput = Reactive::Property<int>();
+bool ledState;
 
 void setup()
 {
-  Serial.begin(115200);
-  while (!Serial)
-    delay(1);
+  pinMode(LED_BUILTIN, OUTPUT);
 
-  //pinMode(LED_BUILTIN, OUTPUT);
+  pinMode(pin2, OUTPUT);
+  pinMode(pin4, OUTPUT);
+  pinMode(pin7, OUTPUT);
+  pinMode(pin8, OUTPUT);
+  pinMode(pin12, OUTPUT);
+}
 
-  observableIntSerialOutput.ToSerial();
+void generalTask(void (*f)())
+{
+  currentMillis = millis(); // store the current time
+  if (currentMillis - previousMillis >= period)
+  {
+    previousMillis = currentMillis; // save the last time you blinked the LED
+    ledState = !ledState;
 
-  reactiveInput
-      .If([](int x) { return x== 1; },
-          [](int x) { Serial.println(10 * x); })
-      .DoNothing();
+    f();
+  }
+}
+
+void task()
+{
+  generalTask([] {
+    digitalWrite(LED_BUILTIN, ledState);
+  });
 }
 
 void loop()
 {
-  reactiveInput.Next();
-}*/
-
-auto reactiveInput = Reactive::FromDigitalInput(10, INPUT_PULLUP);
-
-auto observableManual = Reactive::ManualDefer<int>();
-
-bool isNotified;
-bool isNotified2;
-
-void setup()
-{
-  Serial.begin(115200);
-  while (!Serial)
-    delay(1);
-
-  observableManual
-      .Toggle()
-      .ToDigitalOutput(LED_BUILTIN);
-
-  //observableManual.Next();
-
-  reactiveInput
-      .If([](int x) { return x == HIGH; },
-          [](int x) {
-            if (!isNotified)
-            {
-              Serial.println("La pompa è accesa");
-              isNotified = true;
-              isNotified2 = false;
-              observableManual.Next();
-            }
-          })
-      .If([](int x) { return x == LOW; },
-          [](int x) {
-            if (!isNotified2)
-            {
-              Serial.println("La pompa è spenta");
-              isNotified = false;
-              isNotified2 = true;
-              observableManual.Next();
-            }
-          })
-      .DoNothing();
-}
-
-void loop()
-{
-  reactiveInput.Next();
+  task();
 }
