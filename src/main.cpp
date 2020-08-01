82bd87c0902fa48eb0ae41997788e7b4f9a67cec
federico <applegino9>
2020-08-01 18:08:58 +0200

    reactiveArduino lib

--- src/main.cpp
+++ src/main.cpp
@@ -6,11 +6,9 @@ using namespace Reactive;
 
 //WaterPump waterPump;
 
-const int pin12 = 12;
+/*const int pin12 = 11;
 
 auto reactiveInput = Reactive::FromDigitalInput(pin12, INPUT_PULLUP);
-
-auto observableInt = Reactive::Property<int>();
 
 auto observableIntSerialOutput = Reactive::Property<int>();
 
@@ -20,14 +18,61 @@ void setup()
   while (!Serial)
     delay(1);
 
+  //pinMode(LED_BUILTIN, OUTPUT);
+
   observableIntSerialOutput.ToSerial();
 
-  observableInt
-      .Select([](int x) { return digitalRead(pin12); })
+  reactiveInput
+      .If([](int x) { return x== 1; },
+          [](int x) { Serial.println(10 * x); })
+      .DoNothing();
+}
+
+void loop()
+{
+  reactiveInput.Next();
+}*/
+
+auto reactiveInput = Reactive::FromDigitalInput(10, INPUT_PULLUP);
+
+auto observableManual = Reactive::ManualDefer<int>();
+
+bool isNotified;
+bool isNotified2;
+
+void setup()
+{
+  Serial.begin(115200);
+  while (!Serial)
+    delay(1);
+
+  observableManual
+      .Toggle()
       .ToDigitalOutput(LED_BUILTIN);
 
+  //observableManual.Next();
+
   reactiveInput
-      .If([](int x) { return x == HIGH; }, [](int x) { observableIntSerialOutput = x; })
+      .If([](int x) { return x == HIGH; },
+          [](int x) {
+            if (!isNotified)
+            {
+              Serial.println("La pompa è accesa");
+              isNotified = true;
+              isNotified2 = false;
+              observableManual.Next();
+            }
+          })
+      .If([](int x) { return x == LOW; },
+          [](int x) {
+            if (!isNotified2)
+            {
+              Serial.println("La pompa è spenta");
+              isNotified = false;
+              isNotified2 = true;
+              observableManual.Next();
+            }
+          })
       .DoNothing();
 }
 
